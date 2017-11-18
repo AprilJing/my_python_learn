4e4138d2c87ce3ecc3f390b9f1ca0001c7699345
AprilJing <ljy199341@163.com>
2017-11-18 16:53:10 -0700

    exercise

---
+++ exercise/ex3.py
@@ -0,0 +1,20 @@
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30/6
+# 25*3 then the mode of 4 (3), then 100-3
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print "## warning: in python2, 1/4=0; but in python3, 1/4=0.25"
+print 3+2+1-5+4%2-1.00/4+6 # transfer 1 to 1.00, which becomes a floating number"
+
+print "What is 3+2?", 3+2
+print "What is 5-7?", 5-7
+
+print "Oh, that's why it's false. " \
+      "How about some more." # This could keep the sentence the same line above
+
+print "Is it greater?", 5>-2
+print "Is it greater or equal?", 5>= -2
+print "Is it less or equal?", 5 <= -2
---
+++ exercise/learn.py
@@ -0,0 +1,15 @@
+## ex1
+# -*- coding:utf-8 -*-
+# The line above can show not only English.
+
+print "Hello 我 World!"
+# print "Hello Again"
+# print "I like typing this."
+# print "This is fun."
+# print 'Yay! Printing.'
+# print "I'd much rather you 'not'."
+# print ''
+# print 'I "said" do not touch this.'
+print '\n'
+
+
